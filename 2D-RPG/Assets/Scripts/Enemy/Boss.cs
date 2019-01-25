78f3d79cd916337b5fbdabfc650a3c62d0aef76a
David Larkin <davidlarkin96@gmail.com>
2019-01-25 20:51:17 +0000

    Made helper methods for later use with bosses
    Method created to get all ground tiles for rooms, method to convert a transforms position to relative position, and created a method to get all tiles to left, right, up and down of a transform

--- 2D-RPG/Assets/Scripts/Enemy/Boss.cs
+++ 2D-RPG/Assets/Scripts/Enemy/Boss.cs
@@ -12,6 +12,7 @@ public class Boss : Enemy
     private bool walkingRight;
     private bool attacking;
     protected bool hasWeapon;
+    private GameObject bossRoom;
 
     //protected PolygonCollider2D[] weaponColliders;
     protected Weapon weapon;
@@ -21,10 +22,13 @@ public class Boss : Enemy
     {
         base.Start();
 
+        bossRoom = GameObject.Find("BossRoom1");
+        List<Transform> besideTiles = GetAllTilesStraightLines();
+
+        foreach (Transform tile in besideTiles)
+            Debug.Log(tile.transform.position); // Printing all tiles beside enemy
 
         hasWeapon = (GetComponentInChildren<Weapon>() != null);
-        //Debug.Log(hasWeapon);
-        //hasWeapon = (GetComponentInChildren<PolygonCollider2D>() != null); // has weapon is true if children has PolygonCollider2D
 
         if(hasWeapon)
             weapon = GetComponentInChildren<Weapon>(); // Get Colliders for the weapon
@@ -97,43 +101,38 @@ public class Boss : Enemy
         base.FollowTarget(target);
     }
 
-    /*
-    private void animate()
-    {
-			
-        anim.SetBool("walkingUp", walkingUp);
-        anim.SetBool("walkingDown", walkingDown);
-        anim.SetBool("walkingLeft", walkingLeft);
-        anim.SetBool("walkingRight", walkingRight);
-        anim.SetBool("attacking", attacking);
-        if (Input.GetKey(KeyCode.W))
-            walkingUp = true;
-        else
-            walkingUp = false;
-
-        if (Input.GetKey(KeyCode.S))
-            walkingDown = true;
-        else
-            walkingDown = false;
-
-        if (Input.GetKey(KeyCode.A))
-            walkingLeft = true;
-        else
-            walkingLeft = false;
-
-        if (Input.GetKey(KeyCode.D))
-            walkingRight = true;
-        else
-            walkingRight = false;
-
-    }
-    */
-
     IEnumerator StopAnimation()
     {
         Debug.Log(anim.GetCurrentAnimatorStateInfo(0).length);
         yield return new WaitForSeconds(0.5f);
         attacking = false;
     }
+
+    /*
+     * Method to convert transform position to relative position (fixed) 
+     */
+    Vector2 ConvertAbsPositionToUnitPos(Vector2 pos)
+    {
+        return new Vector2(Mathf.RoundToInt(transform.position.x % 32), Mathf.RoundToInt(transform.position.y % 32));
+    }
+
+    /*
+     * Method to get all tiles to the right, left, above and below transform position
+     */
+    List<Transform> GetAllTilesStraightLines()
+    {
+        Vector2 currentPos = ConvertAbsPositionToUnitPos(transform.position);
+
+        List<Transform> tilesToUse = new List<Transform>();
+        foreach(Transform tile in bossRoom.GetComponent<Tiles>().tiles)
+        {
+            if (tile.transform.position.x == currentPos.x || tile.transform.position.y == currentPos.y)
+                tilesToUse.Add(tile);
+        }
+
+        return tilesToUse;
+
+    }
+
     
 }
---
+++ 2D-RPG/Assets/Tiles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour
+{
+    public List<Transform> tiles;
+
+	void Start ()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name == "Ground")
+            {
+                foreach(Transform groundTile in child)
+                    tiles.Add(groundTile);
+            }
+        }
+
+    }
+}
