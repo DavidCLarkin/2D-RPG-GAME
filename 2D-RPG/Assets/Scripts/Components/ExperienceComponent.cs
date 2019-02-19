07ae5d47be26581dc6b91c9c085ebfe0ffe40cde
David Larkin <davidlarkin96@gmail.com>
2019-02-19 10:40:04 -0500

    Added an experience system, that levels the player up upon hitting a certain amount of exp. Experience is currently gained by pressing a button, later to change.

---
+++ 2D-RPG/Assets/Scripts/Components/ExperienceComponent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ExperienceComponent : MonoBehaviour 
+{
+	public int level = 1;
+	public int currentExp = 0;
+	public int baseExp = 100;
+	public int expToLevel = 100;
+	public float expIncrease = 1.7f;
+
+	public Slider expBar;
+
+	void Start()
+	{
+		expBar.value = CalculateExpPercentage ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.N)) 
+		{
+			IncreaseExp (50);
+		}
+	}
+
+	void IncreaseExp(int exp)
+	{
+		currentExp += exp;
+		if (currentExp >= expToLevel) 
+		{
+			LevelUp();
+		}
+
+		expBar.value = CalculateExpPercentage ();
+	}
+
+	void LevelUp()
+	{
+		currentExp -= expToLevel;
+		level++;
+		expToLevel = (int)(baseExp * (Mathf.Pow (expIncrease, level)));
+	}
+
+	float CalculateExpPercentage()
+	{
+		Debug.Log (currentExp / baseExp);
+		return (float)currentExp / expToLevel;
+	}
+}
